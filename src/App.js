bf87aa181c24c3f59d81d8b0432832c891eed8b3
OsvalDev <osvaworkdev@gmail.com>
2024-03-26 15:46:33 -0600

    UI: login

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
 import React, { useState } from 'react';
+import logo from './assets/carnivalLogo.png'
 function App() {
 
   return (
     <div className="App">
-      <div class="flex w-full lg:w-1/2 justify-center items-center bg-white space-y-8">
-            <div class="w-full px-8 md:px-32 lg:px-24">
-            <form class="bg-white rounded-md shadow-2xl p-5">
-              <h1 class="text-gray-800 font-bold text-2xl mb-1">Hello Again!</h1>
-              <p class="text-sm font-normal text-gray-600 mb-8">Welcome Back</p>
-              <div class="flex items-center border-2 mb-8 py-2 px-3 rounded-2xl">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
-                </svg>
-                <input id="email" class=" pl-2 w-full outline-none border-none" type="email" name="email" placeholder="Email Address" />
-              </div>
-              <div class="flex items-center border-2 mb-12 py-2 px-3 rounded-2xl ">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                  <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
-                </svg>
-                <input class="pl-2 w-full outline-none border-none" type="password" name="password" id="password" placeholder="Password" />
-                
-              </div>
-              <button type="submit" class="block w-full bg-indigo-600 mt-5 py-2 rounded-2xl hover:bg-indigo-700 hover:-translate-y-1 transition-all duration-500 text-white font-semibold mb-2">Login</button>
-              <div class="flex justify-between mt-4">
-                <span class="text-sm ml-2 hover:text-blue-500 cursor-pointer hover:-translate-y-1 duration-500 transition-all">Forgot Password ?</span>
-
-                <a href="#" class="text-sm ml-2 hover:text-blue-500 cursor-pointer hover:-translate-y-1 duration-500 transition-all">Don't have an account yet?</a>
-              </div>
-              
-            </form>
+      <div class="w-screen h-screen flex justify-center items-center bg-gray-50 flex-col">
+            <img src={logo} alt="Carnival logo" className="w-[50vw] md:w-[20vw] mb-6" />
+            <div className="w-fit h-fit flex justify-center items-center flex-col px-8 py-4 rounded-xl shadow-lg bg-white">            
+                <h3 class="w-full font-bold sm:text-3xl text-lg text-center mb-8">Inicio de sesion</h3>    
+                <form class="w-full flex justify-center items-center flex-col">
+                    <div class="mb-5">
+                        <label for="idWorker" class="block mb-2 sm:text-sm text-xs font-medium text-gray-900">Numero de empleado</label>
+                        <input type="number" id="idWorker" name="idWorker" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-fuchsia-500 focus:border-fuchsia-500 block w-[75vw] sm:w-96 p-2.5 " required />
+                    </div>
+                    <div class="mb-5">
+                        <label for="password" class="block mb-2 sm:text-sm text-xs font-medium text-gray-900">Contraseña</label>
+                        <div class="relative">
+                            <input type="password" id="password" name="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-fuchsia-500 focus:border-fuchsia-500 block w-[75vw] sm:w-96 p-2.5 " required />
+                            <div class="absolute inset-y-0 end-0 flex items-center pe-3.5">
+                                <i id="ojito" class='bx bx-show cursor-pointer'></i>
+                            </div>
+                        </div>                    
+                    </div>
+                    <button id="btnLogin" type="button" class="text-white bg-[#C44890] hover:bg-fuchsia-800 focus:ring-4 focus:outline-none focus:ring-fuchsia-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-40">Aceptar</button>                                
+                </form>
             </div>
-            
-          </div>
+        </div>
     </div>
   );
 }
