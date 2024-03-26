d1dbad65e3c622c505a45bbfb7517c9b917cf8a6
OsvalDev <osvaworkdev@gmail.com>
2024-03-26 11:13:39 -0600

    test: axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { io } from 'socket.io-client'
 import logo from './logo.svg';
 import './App.css';
@@ -8,9 +9,24 @@ const socket = io('http://170.1.1.10:3001')
 function App() {
   const [clicks, setClicks] = useState(0);
   const [isConnected, setIsConnected] = useState();
+  const [data, setData] = useState(null);
 
   useEffect( ()=> {
     socket.on('connect', () => setIsConnected(true))  
+  }, []);
+
+  useEffect( ()=> {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://api.waifu.pics/sfw/waifu');
+        console.log(response)
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
   }, []);
 
   const handleClick = () => {
@@ -20,8 +36,8 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header">        
+        <img src = {data ? data.url : logo} className="App-logo" alt='imgAxios'/>
         <p>Total de clics: {clicks}</p>        
         <button className="App-button" onClick={handleClick}>
           Click Me
